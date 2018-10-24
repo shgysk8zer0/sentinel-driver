29e74061435d6660b190e245ec870c712b8b9bbe
Chris Zuber <shgysk8zer0@gmail.com>
2018-10-24 13:42:52 -0700

    Add login form & buttons

--- js/index.js
+++ js/index.js
@@ -4,4 +4,22 @@ import {ready, $} from './std-js/functions.js';
 
 ready().then(async () => {
 	$(document.documentElement).replaceClass('no-js', 'js');
+
+	$('[data-show-modal]').click(event => {
+		$(event.target.closest('[data-show-modal]').dataset.showModal).showModal();
+	}, {
+		passive: true,
+	});
+
+	$('[data-close]').click(event => {
+		$(event.target.closest('[data-close]').dataset.close).close();
+	}, {
+		passive: true,
+	});
+
+	$('form[name="login"]').submit(async event => {
+		event.preventDefault();
+		const body = new FormData(event.target);
+		console.log(Object.fromEntries(body.entries()));
+	});
 });
