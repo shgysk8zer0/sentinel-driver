b6dc4537980b9777cf71126b54e9dea559751893
Chris Zuber <shgysk8zer0@gmail.com>
2018-11-02 12:37:21 -0700

    Add support for PasswordCredential login
    - Store credentials on successful login
    - Get credentials on `<login-button>` click
    - Add `LoginForm.login` & `LoginForm.loginWithCreds` methods

--- components/login-button.js
+++ components/login-button.js
@@ -2,9 +2,13 @@ class HTMLLoginButtonElement extends HTMLButtonElement {
 	constructor() {
 		super();
 		this.hidden = sessionStorage.hasOwnProperty('token');
-		this.addEventListener('click', () => document.querySelector('login-form').showModal());
+		this.addEventListener('click', async () =>  this.form.showModal());
 		document.addEventListener('login', () => this.hidden = true);
 		document.addEventListener('logout', () => this.hidden = false);
+	}
+
+	get form() {
+		return document.querySelector('login-form');
 	}
 }
 
