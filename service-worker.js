03e82788e73b97488fbc9cec1c6332cfc2998701
Chris Zuber <shgysk8zer0@gmail.com>
2018-11-12 11:49:01 -0800

    Add unsupported-browser class to cache

--- service-worker.js
+++ service-worker.js
@@ -41,6 +41,7 @@ const config = {
 		'/components/logout-button.js',
 		'/components/current-year.js',
 		'/components/offline-message.js',
+		'/components/unsupported-browser.js',
 		'/components/vehicle-list/vehicle-list.js',
 		'/components/vehicle-element/vehicle-element.js',
 		'/components/driver-list/driver-list.js',
